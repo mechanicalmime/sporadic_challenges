5e7b19b74085cbead0e4ee1d17add299b16ca40f
Guillermo Garza <mechanical.mime@gmail.com>
2017-11-15 15:01:44 -0600

    Reddit challenge from Nov 13 2017

---
+++ reddit_20171113/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec firstRecurringCharacterIndex (chars:List<char>) (seen:Set<char>) : int =
+    match chars with
+    | [] -> -1
+    | c::_ when (Set.contains c seen) -> Set.count seen
+    | c::cs -> firstRecurringCharacterIndex cs (Set.add c seen)
+
+[<EntryPoint>]
+let main argv =
+    let input = Console.ReadLine()
+    let index = (firstRecurringCharacterIndex (Seq.toList input) Set.empty)
+    Console.WriteLine(index)
+    0 
